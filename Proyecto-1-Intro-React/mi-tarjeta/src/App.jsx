76ab3a264734fe55d1c22a037a6da9d802641cd5
Emilio <EmilianoR117@outlook.com>
2025-07-05 15:13:30 -0700

    feat: Proyecto 1

---
+++ Proyecto-1-Intro-React/mi-tarjeta/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Tarjeta from "./Tarjeta"
+
+function App() {
+
+  return (
+    <div style={{justifyItems: "center"}}>
+      <h1>Tarjeta de Presentación</h1>
+      {/* Renderizamos el componente Tarjeta */}
+      <Tarjeta />
+    </div>   
+  )
+}
+
+export default App
